7106a3fcff745f6afdd2678dcc3112932bdf4502
heronet <sirat4757@gmail.com>
2025-03-10 22:11:09 +0600

    add: more

--- DHT11 + OLED.py
+++ DHT11 + OLED.py
@@ -1,12 +1,9 @@
-# Complete project details at https://RandomNerdTutorials.com/micropython-ssd1306-oled-scroll-shapes-esp32-esp8266/
-
 from machine import Pin, SoftI2C
 import ssd1306
 from time import sleep
-import gfx
 import dht
 
-# ESP32 Pin assignment
+# OLED Pin assignment
 i2c = SoftI2C(scl=Pin(21), sda=Pin(22))
 
 oled_width = 128
@@ -17,26 +14,26 @@ sensor = dht.DHT11(Pin(5))
 
 while True:
   try:
-    oled.fill(0)
-      
-    sleep(1)
-    
     sensor.measure()
     temp = sensor.temperature()
     hum = sensor.humidity()
     temp_f = temp * (9/5) + 32.0
+    
     print('Temperature: %3.1f C' %temp)
     print('Temperature: %3.1f F' %temp_f)
     print('Humidity: %3.1f %%' %hum)
     
-    oled.text('Temp: %3.1f C' %temp, 0, 0)
-    oled.text('Temp: %3.1f F' %temp_f, 0, 10)
-    oled.text('Humidity: %3.1f %%' %hum, 0, 20)
+    oled.fill(0)
+    oled.text("ESP32-WROOM-32", 0, 0)
+    oled.text("Micropython", 0, 10)
+    
+    oled.text('Temp 1: %3.1f C' %temp, 0, 30)
+    oled.text('Temp 2: %3.1f F' %temp_f, 0, 40)
+    oled.text('Humidity: %3.1f %%' %hum, 0, 50)
     
     oled.show()
-
+    sleep(1)
   except OSError as e:
-    print('Failed to read sensor.')
-
-
-
+    oled.text("ESP32-WROOM-32", 0, 0)
+    oled.text("Micropython", 0, 10)
+    oled.text('Failed to read sensor.', 0, 30)
